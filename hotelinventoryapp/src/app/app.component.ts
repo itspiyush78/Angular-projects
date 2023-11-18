56f2a30e131e7d34ebd73b7caf96ab7e8b6c15e7
itspiyush78 <itspiyush78@gmail.com>
2023-11-18 09:32:46 +0530

    rooms added

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
- // templateUrl: './app.component.html',
- template: '<h1>hi folks from inline template!</h1>',
-  styleUrls: ['./app.component.scss']
+  selector: 'hinv-root',
+ templateUrl: './app.component.html',
+
+
+
+styleUrls: ['./app.component.scss']
+// styles: [`h1 {color:red;}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
